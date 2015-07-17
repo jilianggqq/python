309535a26dbd5ec0538ef9de2d5bcf34e7e40984
Peter X230 <PeterX230@itu.edu>
2015-07-16 23:51:05 -0700

    familiar with form operation

--- tango_with_django_project/README.md
+++ tango_with_django_project/README.md
@@ -8,3 +8,7 @@ How To Tango With Django 1.7
 **5**. [Templates and Static Media](http://www.tangowithdjango.com/book17/chapters/templates_static.html)
 
 **6**. [Models and Databases](http://www.tangowithdjango.com/book17/chapters/models.html)
+
+**7**. [ Models, Templates and Views](http://www.tangowithdjango.com/book17/chapters/models_templates.html)
+
+**8**. [Fun with Forms](http://www.tangowithdjango.com/book17/chapters/forms.html)
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index/', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),  # New!
+    url(r'^add_category/$', views.add_category, name='add_category'), 
+    url(r'^add_page/(?P<category_name_slug>[\w\-]+)/$', views.add_page, name='add_page'), 
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 ]
