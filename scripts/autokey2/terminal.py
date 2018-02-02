bf1c48c30b8ff4f7b16186d667fe5451260ef085
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2018-02-02 09:10:25 -0800

    add terminal and atom

--- scripts/autokey2/terminal.py
+++ scripts/autokey2/terminal.py
@@ -1,6 +1,6 @@
 # Enter script code
 if window.wait_for_exist('gqq@gqq-W541: ~', 0) == True:
-    window.activate('gqq@gqq-W541: ~', switchDesktop=False, matchClass=False)
+    window.activate('x-terminal-emulator.X-terminal-emulator', switchDesktop=False, matchClass=True)
     # dialog.info_dialog("test1")
 # else:
     # system.exec_command('/usr/bin/google-chrome', getOutput=True)
