c7321bbfe52fa15c4df923ad2a0ecc28320c4cb1
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2018-02-02 22:33:12 -0800

    modify atom.py

--- scripts/autokey2/atom.py
+++ scripts/autokey2/atom.py
@@ -1,5 +1,5 @@
 # Enter script code
 if window.wait_for_exist('.*— Atom', 0) == True:
-    window.activate('Atom', switchDesktop=False, matchClass=False)
+    window.activate('atom.Atom', switchDesktop=False, matchClass=True)
 else:
     system.exec_command('/usr/bin/atom', getOutput=True)
